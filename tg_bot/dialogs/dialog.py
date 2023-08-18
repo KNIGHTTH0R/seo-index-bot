92eb313bb375264168d571f6376641ae34146431
Krak3nDev <lovekuchernazarlove@gmail.com>
2023-08-18 18:47:40 +0300

    Add admin_menu to dialog.py
    The admin_menu was added to the dialog.py file. This change allows administrators to have a separate dialog from regular users, enabling them to control and oversee the bot's interactions more efficiently.

--- tg_bot/dialogs/dialog.py
+++ tg_bot/dialogs/dialog.py
@@ -1,6 +1,6 @@
 from aiogram_dialog import Dialog
 
-from .windows import main_user_menu_window, order_links, language_menu_window, deposit
+from .windows import main_user_menu_window, order_links, language_menu_window, deposit, admin_menu
 
 
 def bot_menu_dialogs():
@@ -9,4 +9,5 @@ def bot_menu_dialogs():
         Dialog(*order_links()),
         Dialog(language_menu_window()),
         Dialog(*deposit()),
+        Dialog(*admin_menu())
     ]
